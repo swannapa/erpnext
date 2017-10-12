6a6f9b8818a44c777cf1d51f0c6258b1cfe74a51
Wannapa Siengboon <swannapa@simpletech.local>
2017-10-12 08:37:40 +0700

    Tarn Commit

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -709,8 +709,7 @@ def get_advance_journal_entries(party_type, party, party_account, amount_field,
 		from
 			`tabJournal Entry` t1, `tabJournal Entry Account` t2
 		where
-			t1.name = t2.parent and t2.account = %s
-			and t2.party_type = %s and t2.party = %s
+			t1.name = t2.parent and t2.account = %s and t2.party_type = %s and t2.party = %s
 			and t2.is_advance = 'Yes' and t1.docstatus = 1
 			and {1} > 0 {2}
 		order by t1.posting_date""".format(amount_field, dr_or_cr, reference_condition),
